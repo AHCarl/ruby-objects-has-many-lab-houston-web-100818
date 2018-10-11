8da966128b74cd1c54394d1ad04ebed781242e82
Alexander Carlin <ahcarlin@gmail.com>
2018-10-11 22:37:49 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,10 @@
 class Post 
-  attr_accessor :title, :posts
+  attr_accessor :title
   
   @@all = []
   
   def initialize(title)
     self.title = title
-    self.posts = []
     self.all << self
   end
   
